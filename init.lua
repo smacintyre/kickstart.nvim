59e6e7787b43e124592639eea1116cc8d021c84c
Shawn MacIntyre <shawn@selfstructured.com>
2024-09-06 09:35:56 +0700

    feat: adding cloudformation support

--- init.lua
+++ init.lua
@@ -590,7 +590,25 @@ require('lazy').setup({
         terraformls = {},
         tsserver = {},
         tailwindcss = {},
-        yamlls = {},
+        yamlls = {
+          settings = {
+            yaml = {
+              completion = true,
+              hover = true,
+              format_on_save = true,
+              format = {
+                enable = true,
+              },
+              schemas = {
+                ['https://raw.githubusercontent.com/aws/serverless-application-model/main/samtranslator/schema/schema.json'] = '/**/*.template.yaml',
+                -- ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '/**/*.gitlab.yml',
+              },
+              schemaStore = {
+                enable = true,
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
